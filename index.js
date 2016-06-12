65aa30e8547283a2d904d84504d5beb010c2e75d
Jimmy <chioulf2@yahoo.com.tw>
2016-06-12 13:04:33 +0800

    Solve chinese showing problem

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-d3.csv("population.csv", function(data) {
+csv = d3.dsv(",", "text/csv;charset=big5"); //解決讀csv亂碼的方法 自行定義d3裡面的csv讀取
+csv("cluster.csv", function(data) {   //原來適用d3.csv開頭 但要解決亂碼 改方式
   var dataobj = { children: data };
   var pack = d3.layout.pack();
   pack = pack.padding(5).size([800,600]).sort(function(a,b) { return b.value - a.value; });
@@ -15,13 +16,13 @@ d3.csv("population.csv", function(data) {
       cx: function(it) { return it.x; }, // 用 x,y 當圓心
       cy: function(it) { return it.y; },
       r : function(it) { return it.r; }, // 用 r 當半徑
-      fill: function(it) { return color(it.country); },
+      fill: function(it) { return color(it.topic); },
       stroke: "#444",                    // 邊框畫深灰色
     })
     .on('click', function(it){
-        alert("國家:" + it.country + ", 人口:" + it.value);
+        alert("主題:" + it.topic + ", 篇數:" + it.value);
     });
-
+  // alert("中文測試");
   d3.select("svg").selectAll("text").data(nodes).enter()
     .append("text")
     // .style("font-size", function(d) { return d.r/4 + "px"; })
@@ -29,6 +30,6 @@ d3.csv("population.csv", function(data) {
       x: function(it) { return it.x; },
       y: function(it) { return it.y; },
       "text-anchor": "middle",                    // 文字水平置中
-    }).text(function(it)  { return (it.value>60000000 ? it.country : ""); }); // 人數如果大於一定程度，設定文字為國名
+    }).text(function(it)  { return (it.value>30 ? it.topic : ""); }); // 篇數如果大於一定程度，會顯示標題
 
   });
