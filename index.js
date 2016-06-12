d5a9091463468aa4ed5b0cf5903a807625bf2375
Jimmy <chioulf2@yahoo.com.tw>
2016-06-12 17:35:54 +0800

    大改 改掉原本的寫法 並解決一些問題 1.圖超出去的問題(不確定完全解決 只是加高高度突然就可以) 2.打亂順序 3.自隨著球體大小改變 4.字凸出去泡泡的問題

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 csv = d3.dsv(",", "text/csv;charset=big5"); //解決讀csv亂碼的方法 自行定義d3裡面的csv讀取
 csv("cluster.csv", function(data) {   //原來適用d3.csv開頭 但要解決亂碼 改方式
-  var dataobj = { children: data };
-  var pack = d3.layout.pack();
-  pack = pack.padding(5).size([800,600]).sort(function(a,b) { return b.value - a.value; });
-  var nodes = pack.nodes(dataobj);
-  // nodes = nodes.filter(function(it) { return it.parent; });
-  var color = d3.scale.category20();
-  d3.select("svg")
-    .selectAll("circle")                 // 建立 circle 的 Selection
-    .data(nodes)                         // 綁定 selection 與資料
-    .enter()                             // 對於任何沒被對應而落單的資料 ...
-    .append("circle")                    // 新增一個 circle 的html tag
-    .attr("class", "circle")             // 新增叫circle的class (css那有對應的hover動作)
-    .attr({
-      cx: function(it) { return it.x; }, // 用 x,y 當圓心
-      cy: function(it) { return it.y; },
-      r : function(it) { return it.r; }, // 用 r 當半徑
-      fill: function(it) { return color(it.topic); },
-      stroke: "#444",                    // 邊框畫深灰色
-    })
-    .on('click', function(it){
-        alert("主題:" + it.topic + ", 篇數:" + it.value);
-    });
-  // alert("中文測試");
-  d3.select("svg").selectAll("text").data(nodes).enter()
-    .append("text")
-    // .style("font-size", function(d) { return d.r/4 + "px"; })
-    .attr({
-      x: function(it) { return it.x; },
-      y: function(it) { return it.y; },
-      "text-anchor": "middle",                    // 文字水平置中
-    }).text(function(it)  { return (it.value>30 ? it.topic : ""); }); // 篇數如果大於一定程度，會顯示標題
+  var bleed = 100;
+  var width = 960;
+  var height = 960;
 
+  var dataobj = { children: data };
+  var pack = d3.layout.pack()
+    .sort(null)
+    .size([width, height + bleed * 2])
+    .padding(2);
+  // .sort(function(a,b) { return b.value - a.value; });  // 逆時針 中間由大到小的排序方式
+  
+  var color = d3.scale.category20();
+  var svg = d3.select("#root")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(0," + -bleed + ")");
+
+  var node = svg.selectAll(".node")
+      .data(pack.nodes(dataobj)
+      .filter(function(d) { return !d.children; }))
+      .enter()
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("circle")
+      .attr("class", "circle")             // 新增叫circle的class (css那有對應的hover動作)
+      .attr({
+        r : function(it) { return it.r; }, // 用 r 當半徑
+        fill: function(it) { return color(it.topic); },
+        stroke: "#444",                    // 邊框畫深灰色
+      })
+      .on('click', function(it){
+        alert("主題:" + it.topic + ", 篇數:" + it.value);
+      });
+
+  node.append("text")
+      .text(function(it)  { return (it.value>30 ? it.topic : ""); }) // 篇數如果大於一定程度，會顯示標題
+      .style("font-size", function(d) { return Math.min(2 * d.r, (2 * d.r - 8) / this.getComputedTextLength() * 24) + "px"; })
+      .attr("dy", ".35em");
   });
+
+  // var nodes = pack.nodes(dataobj);
+  // nodes = nodes.filter(function(it) { return it.parent; });
+  // svg.selectAll("circle")                 // 建立 circle 的 Selection
+  //   .data(nodes)                         // 綁定 selection 與資料
+  //   .enter()                             // 對於任何沒被對應而落單的資料 ...
+  //   .append("circle")                    // 新增一個 circle 的html tag
+  //   .attr("class", "circle")             // 新增叫circle的class (css那有對應的hover動作)
+  //   .attr({
+  //     cx: function(it) { return it.x; }, // 用 x,y 當圓心
+  //     cy: function(it) { return it.y; },
+  //     r : function(it) { return it.r; }, // 用 r 當半徑
+  //     fill: function(it) { return color(it.topic); },
+  //     stroke: "#444",                    // 邊框畫深灰色
+  //   })
+  //   .on('click', function(it){
+  //       alert("主題:" + it.topic + ", 篇數:" + it.value);
+  //   });
+  // // alert("中文測試");
+  // d3.select("svg").selectAll("text").data(nodes).enter()
+  //   .append("text")
+  //   // .style("font-size", function(d) { return d.r/4 + "px"; })
+  //   .attr({
+  //     x: function(it) { return it.x; },
+  //     y: function(it) { return it.y; },
+  //     "text-anchor": "middle",                    // 文字水平置中
+  //   }).text(function(it)  { return (it.value>30 ? it.topic : ""); }); // 篇數如果大於一定程度，會顯示標題
+
+  // });
